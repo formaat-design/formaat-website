69271da5e118de85a7d7a6677771e4cec70988f5
Dmitry Belyaev <blv.dmitry@gmail.com>
2022-08-22 21:41:23 +0200

    Image ratio

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Text, Actionable, Image } from "reshaped";
+import { Stack, Text, Actionable, Image, AspectRatio } from "reshaped";
 import * as T from "./ProjectCard.types";
 
 const ProjectCard = (props: T.Props) => {
@@ -12,12 +12,14 @@ const ProjectCard = (props: T.Props) => {
       attributes={href ? { target: "_blank" } : undefined}
     >
       <Stack gap={4} direction={{ s: "row", m: "column" }}>
-        <Image
-          borderRadius="medium"
-          src={thumbnail}
-          alt={alt}
-          width={{ s: "100px", m: "100%" }}
-        />
+        <AspectRatio ratio={4 / 3}>
+          <Image
+            borderRadius="medium"
+            src={thumbnail}
+            alt={alt}
+            width={{ s: "100px", m: "100%" }}
+          />
+        </AspectRatio>
         <Stack.Item grow>
           <Stack gap={1}>
             <Text variant="body-medium-1">{title}</Text>
