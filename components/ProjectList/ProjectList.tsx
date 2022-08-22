8ff51e34cf56783bfb81044d063dc7d829f6294d
Dmitry Belyaev <blv.dmitry@gmail.com>
2022-08-22 21:36:41 +0200

    Updated images

--- components/ProjectList/ProjectList.tsx
+++ components/ProjectList/ProjectList.tsx
@@ -10,7 +10,7 @@ const ProjectList = () => {
           href="https://reshaped.so"
           title="Reshaped"
           description="Design system built for your scale in React & Figma"
-          thumbnail="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80"
+          thumbnail="/img/studio/thumbnail-reshaped.png"
           alt="Reshaped components preview"
         />
       </Stack.Item>
@@ -19,7 +19,7 @@ const ProjectList = () => {
           href="https://www.figma.com/community/plugin/1132648122057236517/Reshaped"
           title="Reshaped tokens"
           description="Free Figma plugin to manage themes and dark mode"
-          thumbnail="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80"
+          thumbnail="/img/studio/thumbnail-reshaped-tokens.png"
           alt="Figma plugin interface"
         />
       </Stack.Item>
@@ -28,8 +28,8 @@ const ProjectList = () => {
           href="https://designsystemchecklist.com"
           title="Design system checklist"
           description="An open-source checklist to help you plan, build and grow your design system"
-          thumbnail="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=80"
           alt="Checklist interface"
+          thumbnail="/img/studio/thumbnail-checklist.png"
         />
       </Stack.Item>
     </Stack>
